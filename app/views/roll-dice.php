ec1b18df8721ed5cb34bb317bd59298e57d6eaf1
pkuzma101 <kuzma.paul@gmail.com>
2014-12-01 16:07:14 -0600

    Added win/lose messaging

--- app/views/roll-dice.php
+++ app/views/roll-dice.php
@@ -6,6 +6,14 @@
 <body>
     <h1><?php echo "The Dice say " . $roll; ?></h1>
     <h1><?php echo "Your guess was " . $guess; ?></h1>
+    <h2>
+    	<?php if($roll == $guess): ?>
+    		<p>You win!</p>
+    	<?php endif ?>
+    	<?php if($roll != $guess): ?>
+    		<p>You lose!</p>
+    	<?php endif ?>
+    </h2> 
     
 </body>
 </html>
