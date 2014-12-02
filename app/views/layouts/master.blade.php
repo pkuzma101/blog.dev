2a9df7950263b2431ac489463eaf67f83f54986f
pkuzma101 <kuzma.paul@gmail.com>
2014-12-02 09:47:51 -0600

    First master.blade template - added bootstrap css, bootstrap javascript, and jquery

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Blog</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    @yield('content')
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+</body>
+</html>
