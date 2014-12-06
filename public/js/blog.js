b2bc7b3d2a3c490ca222a9a0e6726dfcf34e40f1
pkuzma101 <kuzma.paul@gmail.com>
2014-12-06 13:50:01 -0600

    Added jQuery and JS scripts - jQuery slide functionality not successful

---
+++ public/js/blog.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+	$(".blog-entry").click(function() {
+		$(this).animate({
+			height: 'auto'
+		});
+	});
+
+	$(".delete-btn").click(function() {
+		var postId = $(this).data('post-id');
+		$("#delete-form").attr('action', '/posts/' + postId);
+		if(!confirm('Are you sure you want to delete this post?')) {
+			$("#delete-form").submit();
+		}
+	});
+
+});
+
