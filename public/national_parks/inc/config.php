66fc412000a21ab28387c329696bb091eb336b17
pkuzma101 <kuzma.paul@gmail.com>
2014-12-10 19:54:25 -0600

    no message

--- public/national_parks/inc/config.php
+++ public/national_parks/inc/config.php
@@ -2,8 +2,8 @@
 
 define ('DB_HOST', '127.0.0.1');
 define ('DB_NAME', 'address_book');
-define ('DB_USER', 'pkuzma101');
-define ('DB_PASS', 'duster2');
+define ('DB_USER', 'foo');
+define ('DB_PASS', 'bar');
 
 require 'db_connect.php';
 
