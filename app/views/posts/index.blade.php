b164b4d47f2f285d0383b8aed5b6587e8f41f225
pkuzma101 <kuzma.paul@gmail.com>
2014-12-10 19:10:37 -0600

    Fixed a bug with the scrolling with an image with an entry

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -22,7 +22,7 @@
 				</p>
 				@if(isset($post->image))
 				<p>
-					<img src="{{{ asset($post->image) }}}" class="blog-image">
+					<img src="{{{ asset($post->image) }}}" class="blog-image" class="blog-entry">
 				</p>
 				@endif
 			</div>
