f5ef1f6b74c5ad2d5648b82ad2484624cf585dc0
pkuzma101 <kuzma.paul@gmail.com>
2015-01-21 18:38:33 -0500

    Navbar now has dynamic resizability

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,56 +1,43 @@
-<nav class="navbar navbar-default" role="navigation">
-	<div class="container-fluid">
-	 <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-	    	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	        	<ul class="nav navbar-nav">
-		        	<li class="active">
-		        		<a href="/posts">Home<span class="sr-only">(current)</span>
-		        		</a>
-		        	</li>
-		        	<li>
-		        		@if (Auth::check())
-		        		<a href="/posts/create">New<span class="sr-only">(current)</span></a>
-		        		@endif
-		        	</li>
-		        	<li>
-		        		<a href="/resume">Resume<span class="sr-only">(current)</span></a>
-		        	</li>
-		        	<li>
-		        		<a href="/portfolio">Portfolio<span class="sr-only">(current)</span></a>
-		        	</li>
-		        	<li>
-		        		<a href="https://github.com/pkuzma101">GitHub<span class="sr-only">(current)</span></a>
-		        	</li>
-		        	<li>
-		        		<a href="https://www.linkedin.com/pub/paul-kuzma/a9/a7b/170/">LinkedIn<span class="sr-only">(current)</span></a>
-		        	</li>
-		        </ul>
-		        <!-- Search Bar goes here -->
-		        <form class="navbar-form navbar-left" role="search">
-		        	<div class="form-group">
-          				<input type="text" class="form-control" name="search" placeholder="Search Posts">
-          			<button type="submit" class="btn btn-default">Search</button>
-          			</div>
-		        </form>
-		        <!-- Login code goes here -->
-		        <ul class="nav navbar-nav navbar-right">
-		        	@if(Auth::guest())
-		        	<li>
-		        		<a href="{{{ action('HomeController@showLogin') }}}">Login<span class="sr-only">(current)</span></a>
-		        	</li>
-		        	@else
-		        	<li id="user-email">
-		        		<p>
-		        			
-		        		</p>
-		        	</li>
-		        	<li>
-		        		<a href="{{{ action('HomeController@doLogout') }}}">Logout</a>
-		        	</li>
-		        	@endif
-	       		</ul>
-	        </div><!-- /.navbar-collapse -->
-	    </div><!-- /.container-fluid -->
-	</div>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+    	</div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	        <ul class="nav navbar-nav">
+	            <li><a href="/posts">Home</a></li>
+	            <li>
+	                @if (Auth::check())
+	                <a href="/posts/create">New<span class="sr-only">(current)</span></a>
+	                @endif
+	            </li>
+	            <li><a href="/resume">Resume</a></li>
+	            <li><a href="/portfolio">Portfolio</a></li>
+	            @if(Auth::guest())
+	            <li>
+	                <a href="{{{ action('HomeController@showLogin') }}}">Login</a>
+	            </li>
+	            @else
+	            <li>
+	                <a href="{{{ action('HomeController@doLogout') }}}">Logout</a>
+	            </li>
+	            @endif
+	            <li class="dropdown">
+	                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Links<span class="caret"></span></a>
+	                <ul class="dropdown-menu" role="menu">
+	                    <li><a href="https://www.linkedin.com/pub/paul-kuzma/a9/a7b/170/">LinkedIn</a></li>
+	                    <li><a href="https://github.com/pkuzma101">GitHub</a></li>
+	                </ul>
+	            </li>
+	        </ul>
+	    </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
