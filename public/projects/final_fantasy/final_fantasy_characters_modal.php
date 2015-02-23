829a3c70dfb8168f4fec217157c771ca58c1f41b
pkuzma101 <kuzma.paul@gmail.com>
2015-02-23 11:20:19 -0500

    Fixed a bug in the form entry

--- public/projects/final_fantasy/final_fantasy_characters_modal.php
+++ public/projects/final_fantasy/final_fantasy_characters_modal.php
@@ -21,26 +21,26 @@
                         $action = "http://blog.dev/projects/final_fantasy/final_fantasy_characters.php".$page;
                     }
                 ?>
-                <form method="POST" role="form" enctype="multipart/form-data" action="<?php echo $action; ?>" >
+                <form method="POST" role="form" id="add-character-form" enctype="multipart/form-data" action="<?php echo $action; ?>" >
                     <h2 id="modalHeadline">Add New Character</h2>
                     <div class="inputGroup">
-                        <input type="text" name="first_name" id="first_name" class="inputText" value="<?= isset($_POST['first_name']) ? $_POST['first_name'] : '' ?> "></input>
+                        <input type="text" name="first_name" id="first_name" class="inputText" value="<?= isset($_POST['first_name']) ? $_POST['first_name'] : '' ?>"></input>
                         <label for="first_name">First Name</label>
                     </div>
                     <div class="inputGroup">
-                        <input type="text" name="last_name" id="last_name" class="inputText" value="<?= isset($_POST['last_name']) ? $_POST['last_name'] : '' ?> ">
+                        <input type="text" name="last_name" id="last_name" class="inputText" value="<?= isset($_POST['last_name']) ? $_POST['last_name'] : '' ?>">
                         <label for="last_name">Last Name</label>    
                     </div>
                     <div class="inputGroup">
-                        <input type="text" name="class" id="class" class="inputText" value="<?= isset($_POST['class']) ? $_POST['class'] : '' ?> ">
+                        <input type="text" name="class" id="class" class="inputText" value="<?= isset($_POST['class']) ? $_POST['class'] : '' ?>">
                         <label for="class">Class</label>
                     </div>
                     <div class="inputGroup">
-                        <input type="text" name="special_ability" id="special_ability" class="inputText" value="<?= isset($_POST['special_ability']) ? $_POST['special_ability'] : '' ?> ">
+                        <input type="text" name="special_ability" id="special_ability" class="inputText" value="<?= isset($_POST['special_ability']) ? $_POST['special_ability'] : '' ?>">
                         <label for="first_name">Special Ability</label>
                     </div>
                     <div class="inputGroup">
-                        <input type="text" name="weapon" id="weapon" class="inputText" value="<?= isset($_POST['weapon']) ? $_POST['weapon'] : '' ?> ">
+                        <input type="text" name="weapon" id="weapon" class="inputText" value="<?= isset($_POST['weapon']) ? $_POST['weapon'] : '' ?>">
                         <label for="first_name">Weapon</label>    
                     </div>
                     <div class="inputGroup">
