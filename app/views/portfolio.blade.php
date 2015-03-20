a3ee5ecd9f661666d39381d258cc8e7a500e5655
pkuzma101 <kuzma.paul@gmail.com>
2015-03-20 13:58:49 -0500

    Fixed a position bug in hover squares

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -13,7 +13,7 @@
 				<div class="col-md-4" id="portMentSquare">
 					<a href="http://worldmentr.org">
 						<img src="/css/images/mentr.png" class="portPic">
-						<span class="portHover" id="portMentHover">
+						<span class="portHover center-block" id="portMentHover">
 							<p class="portAppName">World Mentor</p>
 							<p class="portAppContent">
 								An interactive website where people can give or receive mentoring advice across the web.
@@ -28,7 +28,7 @@
 				<div class="col-md-4" id="portWhackSquare">
 					<a href="/projects/whackamole/puppy.html">
 						<img src="/css/images/whack.png" class="portPic">
-						<span class="portHover" id="portWhackHover">
+						<span class="portHover center-block" id="portWhackHover">
 							<p class="portAppName">Pet All Puppies</p>
 							<p class="portAppContent">
 								This app utilizes JavaScript and jQuery to create
@@ -40,7 +40,7 @@
 				<div class="col-md-4" id="portFantasySquare">
 					<a href="/projects/final_fantasy/final_fantasy_characters.php">
 						<img src="/css/images/fantasy.png" class="portPic">
-						<span class="portHover" id="portFantasyHover">
+						<span class="portHover center-block" id="portFantasyHover">
 							<p class="portAppName">Character Database</p>
 							<p class="portAppContent">
 								A database that stores information about characters in a 
@@ -52,7 +52,7 @@
 				<div class="col-md-4" id="portTodoSquare">
 					<a href="/projects/todo_list/sql_todo_list.php">
 						<img src="/css/images/todo.png" class="portPic">
-						<span class="portHover" id="portTodoHover">
+						<span class="portHover center-block" id="portTodoHover">
 							<p class="portAppName">To-Do List</p>
 							<p class="portAppContent">
 								A digital things-to-do list that uses MySQL to add and subtract tasks. 
@@ -66,7 +66,7 @@
 				<div class="col-md-4" id="portAlphabetSquare">
 					<a href="/projects/alphabet_soup/alphabet_soup.php">
 						<img src="/css/images/alphabet.png" class="portPic">
-						<span class="portHover" id="portAlphabetHover">
+						<span class="portHover center-block" id="portAlphabetHover">
 							<p class="portAppName">Alphabet Soup</p>
 							<p class="portAppContent">
 								An app that takes any string(s) fed to it and rearranges the letters in alphabetical order.
@@ -77,7 +77,7 @@
 				<div class="col-md-4" id="portNationalSquare">
 					<a href="/projects/national_parks/national_parks.php">
 						<img src="/projects/national_parks/img/canoe.jpg" class="portPic">
-						<span class="portHover" id="portNationalHover">
+						<span class="portHover center-block" id="portNationalHover">
 							<p class="portAppName">National Parks Database</p>
 							<p class="portAppContent">
 								An app that uses MySQL to store information about various national parks in a table.
@@ -88,7 +88,7 @@
 				<div class="col-md-4" id="portPalindromeSquare">
 					<a href="/projects/palindrome/palindrome.php">
 						<img src="/css/images/palindrome.png" class="portPic">
-						<span class="portHover" id="portPalindromeHover">
+						<span class="portHover center-block" id="portPalindromeHover">
 							<p class="portAppName">Palindrome</p>
 							<p class="portAppContent">
 								An app that takes an entered string and judges whether or not it is a palindrome.
