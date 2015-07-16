e192b85450eedff27e93c4f8e3d7ca209b4db2bb
pkuzma101 <kuzma.paul@gmail.com>
2015-07-16 11:08:37 -0400

    Fixed a bug in CSS

--- public/projects/alphabet_soup/alphabet_soup.php
+++ public/projects/alphabet_soup/alphabet_soup.php
@@ -14,31 +14,54 @@ function alphabetSoup($string) {
 
 ?>
 
-<html>		
-	<head>		
-		<title>Alphabet Soup</title>		
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">		
-		<link rel="stylesheet" href="css/alphabet_soup.css">		
-	</head>		
-	<body>		
-		<a href="/portfolio" class="btn btn-default" id="back">Back</a>		
-		<div class="container-fluid" id="soupPage">		
-			<h1>Alphabet Soup</h1>		
-			<div class="container-fluid" id="soupForm">		
-				<form method="POST" role="form" action="/alphabet_soup.php">		
-					<input id="entry" name="entry" type="text" placeholder="Enter text here"></input>		
-					<button type="submit" class="btn btn-primary">Submit</button>		
-				</form>		
-				<div class="answerBox">		
-					<!-- <? foreach($soupWord as $key => $value): ?> -->		
-					<!-- <?= htmlspecialchars(strip_tags($value)); ?> -->		
-					<!-- <? endforeach ?> -->		
-					<? echo alphabetSoup($_POST['entry']); ?>		
-				<div>		
-			</div>		
-		
-		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>		
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>		
-	</body>		
+<html>
+	<head>
+		<title>Alphabet Soup</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link href='http://fonts.googleapis.com/scss?family=Permanent+Marker|Sniglet:400,800|Walter+Turncoat|Coming+Soon' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/alphabet_soup.css">
+		<!-- <link rel="stylesheet" href="css/alphabet.scss"> -->
+	</head>
+	<body>
+		<div class="container-fluid" id="soupPage">
+			<a href="/portfolio" class="btn btn-default" id="back">Back</a>
+			<h1 id="soupTitle">Alphabet Soup</h1>
+			<div class="container-fluid" id="soupForm">
+				<div class="space-filler"></div>
+				<div class="instructions">
+					<p id="">
+						Write in any word and see the letters rearranged in alphabetical order.  
+						You can even write more than one word, and each word will come back alphabetized.
+					</p>
+				</div>
+				<div class="space-filler"></div>
+				<div class="exampleSectionTrigger">
+					<p id="exampleTrigger">
+						<a>For Example...</a>
+					</p>
+				</div>
+				<div class="exampleSectionReveal">
+					<p id="exampleReveal">"hello world" becomes "ehllo dlorw"</p>
+				</div>
+				<div class="formSection">	
+					<form method="POST" role="form" action="" id="soupForm">
+						<input id="entry" name="entry" type="text" placeholder="Enter text here"></input>
+						<button type="submit" id="button" class="btn btn-primary">Submit</button>
+					</form>
+				</div>
+			</div>
+			<div class="answerBox">
+				<p id="answer">
+					<? 
+						if(isset($_POST['entry'])) {
+							echo alphabetSoup($_POST['entry']) . PHP_EOL;
+						}
+					?>
+				</p>
+			</div>	
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+		<script src="/projects/alphabet_soup/js/alphabet_soup.js"></script>
+	</body>
 </html>
