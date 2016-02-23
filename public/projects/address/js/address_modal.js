4efdb52fc54325732644a4fd4ea0e8fec8956e71
pkuzma101 <kuzma.paul@gmail.com>
2016-02-23 13:07:18 -0500

    bug testing

--- public/projects/address/js/address_modal.js
+++ public/projects/address/js/address_modal.js
@@ -1,8 +1,14 @@
-function address_modal(lead_id) {
+function address_modal(person_id) {
 
 	// Empties contents of any previous modals
   $('.modal-title').empty();
   $('.modal-body').empty();
   $('.modal-footer').empty();
-  $('.modal-title').append($('<h3 />', {html: 'Appointments'}));
+  $('.modal-title').append($('<h3 />', {html: 'New Address'}));
+
+  if (person_id) {
+  	var p_id = person_id;
+  }
+
+  console.log(person_id);
 }
