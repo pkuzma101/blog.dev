f12b211aeb94259e4688133b7ef8b931b5bc7d4c
pkuzma101 <kuzma.paul@gmail.com>
2016-02-24 11:13:51 -0500

    testing if edit modal can find appropriate table row

--- public/projects/address/js/address_modal.js
+++ public/projects/address/js/address_modal.js
@@ -19,17 +19,37 @@ function address_modal(person_id) {
   	dataType: 'json',
   	success: function(data) {
   		$('.modal-body').append($('<form />', {id: 'edit_address_form'}));
-  		$('#edit_address_form').append($('<input />', {type: 'hidden', value: data.person_id}));
+  		$('#edit_address_form').append($('<input />', {type: 'hidden', value: data.person_id, id: 'person_id'}));
   		$('#edit_address_form').append($('<div />').append($('<label />', {html: 'First Name: '})).append($('<input />', {type: 'text', id: 'fname', name: 'fname', value: data.fname})));
   		$('#edit_address_form').append($('<div />').append($('<label />', {html: 'Last Name: '})).append($('<input />', {type: 'text', id: 'lname', name: 'lname', value: data.lname})));
   		$('#edit_address_form').append($('<div />').append($('<label />', {html: 'Street: '})).append($('<input />', {type: 'text', id: 'street', name: 'street', value: data.street})));
   		$('#edit_address_form').append($('<div />').append($('<label />', {html: 'City: '})).append($('<input />', {type: 'text', id: 'city', name: 'city', value: data.city})));
   		$('#edit_address_form').append($('<div />').append($('<label />', {html: 'State: '})).append($('<input />', {type: 'text', id: 'state', name: 'state', value: data.state})));
   		$('#edit_address_form').append($('<div />').append($('<label />', {html: 'Zip: '})).append($('<input />', {type: 'text', id: 'zip', name: 'zip', value: data.zip})));
-  		$('#edit_address_form').append($('<div />').append($('<button />', {html: 'Submit', type: 'submit', class: 'address_modal_btn', id: 'edit_submit'})).append($('<button data-dismiss="modal" />', {html: 'Cancel', type: 'button', class: 'address_modal_btn'})));
+  		$('#edit_address_form').append($('<div />').append($('<button />', {html: 'Submit', type: 'submit', class: 'address_modal_btn', id: 'edit_submit'})).append($('<button data-dismiss="modal" class="address_modal_btn">Cancel</button>'})));
 
   		$('#edit_address_form').on('submit', function() {
+  			var params = edit_params + "&" + $(this).serialize();
 
+        var person_id = $('#person_id').val();
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        var street = $('#street').val();
+        var city = $('#city').val();
+        var state = $('#state').val();
+        var zip = $('#zip').val();
+
+        $.ajax({
+          url: url,
+          type: 'post',
+          data: params,
+          dataType: 'json',
+          success: function(json) {
+            console.log($('tr#' + json.person_id).children());
+          }
+        });
+
+  			
   		});
   	}
   });
