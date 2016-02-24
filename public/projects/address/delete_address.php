05951a19f106de83cf0979576e638276ee641e21
pkuzma101 <kuzma.paul@gmail.com>
2016-02-24 16:00:45 -0500

    replaced concatenation mistakes

--- public/projects/address/delete_address.php
+++ public/projects/address/delete_address.php
@@ -4,10 +4,10 @@ require '../../../config.address.php';
 
 $person_id = $_GET['id'];
 
-$deleteAddress = $dbc->prepare("DELETE FROM address WHERE person_id = " + $person_id);
+$deleteAddress = $dbc->prepare("DELETE FROM address WHERE person_id = " . $person_id);
 $deleteAddress->execute();
 
-$deletion = $dbc->prepare("DELETE FROM person WHERE person_id = " + $person_id);
+$deletion = $dbc->prepare("DELETE FROM person WHERE person_id = " . $person_id);
 $deletion->execute();
 
 echo json_encode(array(
