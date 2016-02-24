3440e5eaead98f11c410e28d87226f4c18398bc3
pkuzma101 <kuzma.paul@gmail.com>
2016-02-24 16:04:20 -0500

    debugging

--- public/projects/address/delete_address.php
+++ public/projects/address/delete_address.php
@@ -4,10 +4,15 @@ require '../../../config.address.php';
 
 $person_id = $_GET['id'];
 
+echo '<h1>' . $person_id . '</h1>';
+exit();
+
 $deleteAddress = $dbc->prepare("DELETE FROM address WHERE person_id = " . $person_id);
+$deleteAddress->bindValue(':person_id', $person_id, PDO::PARAM_INT);
 $deleteAddress->execute();
 
 $deletion = $dbc->prepare("DELETE FROM person WHERE person_id = " . $person_id);
+$deletion->bindValue(':person_id', $person_id, PDO::PARAM_INT);
 $deletion->execute();
 
 echo json_encode(array(
