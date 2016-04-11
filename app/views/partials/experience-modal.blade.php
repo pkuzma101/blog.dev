4066b852885a51d31c54be894685c8cf4485c009
pkuzma101 <kuzma.paul@gmail.com>
2016-04-11 12:15:44 -0400

    updated the experience modal

--- app/views/partials/experience-modal.blade.php
+++ app/views/partials/experience-modal.blade.php
@@ -8,7 +8,14 @@
                 <h2 class="contact-list">Experience</h2>
             </div>
             <div id="modal-portfolio" class="container">
-                <p class="exp-heading">Web Developer - 2014 to present</p>
+                <p class="exp-heading">Front End Developer - July 2015 to February 2016</p>
+                  <p class="exp-body">
+                    I worked as the Front End Developer for <em>Main Street Power Mail</em>, a Carmel, Indiana company that sells leads to insurance companies.  I used JavaScript, jQuery, HTML5, and CSS3 to dynamically create an elegant, responsive front end experience.  In addition, I did back end work with MySQL using PHP and Ruby on Rails.
+                  </p>
+                <p class="exp-heading">PHP Developer - March 2015 to July 2015</p>
+                <p class="exp-body">
+                  I worked as a PHP Developer with <em>End Result</em>, a Carmel, Indiana company that records times and scores for marathons and bike races.  I and one other developer used PHP, MySQL, HTML5, and CSS3 to create an online race calendar where users could create races and then add events and information to them.
+                </p>
                 <p class="exp-body">
                   I am a graduate of <em>CodeUp</em>, a programming boot camp in San Antonio, Texas, where I was taught the full LAMP stack, as well as JavaScript, jQuery, HTML, and CSS.  Over a 12-week period, I completed over a hundred coding exercises, homework assignments, and challenges.  I also helped to develop worldmentr.org, an interactive website that allows men and women to give or receive mentoring on any subject across the web.
                 </p>
