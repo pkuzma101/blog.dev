9b3acccfd851f45d8111c81864bded257af4f1ea
pkuzma101 <kuzma.paul@gmail.com>
2017-02-17 12:23:13 -0500

    trying to create generation markers

--- public/projects/timeline/timeline.php
+++ public/projects/timeline/timeline.php
@@ -4,19 +4,19 @@
 <body>
   <section id="timeline_box">
     <ul id="timeline">
-      <?php
-      $current_year = date("Y");
-      /*for ($i = 1967; $i < $current_year + 1; $i++) {
-        echo '<p id="'.$i.'"></p>';
-      }*/
-      ?>
     </ul>
   </section>
   <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
   <script>
     $(document).ready(function() {
+      // create the years of the timeline
       for (i = 1967; i < 2018; i++) {
         $('ul#timeline').append('<li id="'+ i + '"><div class="bubble"><div class="year">'+ i + '</div></div></li>');
+        // create markers for generations
+        if (i == 1972 || i == 1973 || i == 1974 || i == 1975 || i == 1976) {
+          // $('li#'+i).addClass('first_gen');
+          $('li#'+i).prepend('<span class="first_gen"></span>');
+        }
       }
     });
   </script>
