bcd9f23d57ad2413b48de236eaf19ce0115294d8
pkuzma101 <kuzma.paul@gmail.com>
2017-02-20 11:04:07 -0500

    added first generation

--- public/projects/timeline/timeline.php
+++ public/projects/timeline/timeline.php
@@ -3,8 +3,10 @@
 <link rel="stylesheet" href="css/timeline.css" type="text/css">
 <body>
   <section id="timeline_box">
-    <ul id="timeline">
-    </ul>
+    <div id="timeline_div">
+      <ul id="gen_line"></ul>
+      <ul id="timeline"></ul>
+    </div>
   </section>
   <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
   <script>
@@ -12,12 +14,20 @@
       // create the years of the timeline
       for (i = 1967; i < 2018; i++) {
         $('ul#timeline').append('<li id="'+ i + '"><div class="bubble"><div class="year">'+ i + '</div></div></li>');
-        // create markers for generations
-        if (i == 1972 || i == 1973 || i == 1974 || i == 1975 || i == 1976) {
-          // $('li#'+i).addClass('first_gen');
-          $('li#'+i).prepend('<span class="first_gen"></span>');
+      }
+
+      // create years for generation spans
+      for (j = 1967; j < 2018; j++) {
+        $('ul#gen_line').append('<li id="' + j + '"></li>');
+        if (j == 1973 || j == 1974 || j == 1975 || j == 1976 || j == 1977 ) {
+          $('li#' + j).css("background-color", "green");
         }
       }
+
+      // create first generation span
+      // for (a = 1972; a < 1979; a++) {
+      //   $('ul#first_gen').prepend('<li id="first' + a + '" class=""');
+      // }
     });
   </script>
 </body>
