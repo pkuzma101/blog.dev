6ababfe557233010f14376fd61f0fcb0907f60d1
pkuzma101 <kuzma.paul@gmail.com>
2017-03-22 13:54:03 -0400

    testing

--- public/projects/timeline/timeline.php
+++ public/projects/timeline/timeline.php
@@ -14,20 +14,27 @@
       // create the years of the timeline
       for (i = 1967; i < 2018; i++) {
         $('ul#timeline').append('<li id="'+ i + '"><div class="bubble"><div class="year">'+ i + '</div></div></li>');
+        // if (i == 1977) {
+          // console.log($('ul#timeline').children());
+        //   // $('li#' + i).append('<p>sadfsadf</p>');
+        // }
       }
 
       // create years for generation spans
       for (j = 1967; j < 2018; j++) {
         $('ul#gen_line').append('<li id="' + j + '"></li>');
+
+        // first generation span
         if (j == 1973 || j == 1974 || j == 1975 || j == 1976 || j == 1977 ) {
           $('li#' + j).css("background-color", "green");
         }
-      }
 
-      // create first generation span
-      // for (a = 1972; a < 1979; a++) {
-      //   $('ul#first_gen').prepend('<li id="first' + a + '" class=""');
-      // }
+        if (j == 1978 || j == 1979 || j == 1980 || j == 1981 || j == 1982 || j == 1983 ) {
+          $('li#' + j).css("background-color", "orange");
+        }
+      }
+      // $('li#1977').append('<p>sadfsadf</p>');
+
     });
   </script>
 </body>
